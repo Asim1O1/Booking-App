9bb2f3e4ed66821985cba8db54127133e4f5a28e
Asim1O1 <146157042+Asim1O1@users.noreply.github.com>
2024-08-24 21:40:15 +0545

    calendar comp inital design

--- Booking/src/components/calendar.jsx
+++ Booking/src/components/calendar.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendar } from '@fortawesome/free-solid-svg-icons';
 
+const Calendar = () => {
+  const [startDate, setStartDate] = useState(null);
 
-const Calendar = ()=>
-{
+  const handleDateChange = (date) => {
+    setStartDate(date);
+    // You can perform any action with the selected date here
+  };
 
+  return (
+    <div className="calendar-container">
+      <label className="calendar-label">
+        <FontAwesomeIcon icon={faCalendar} /> Select Date
+      </label>
+      <DatePicker
+        selected={startDate}
+        onChange={handleDateChange}
+        className="calendar-input"
+        placeholderText="Choose a date"
+        dateFormat="dd/MM/yyyy"
+        minDate={new Date()} // Disable past dates
+      />
+    </div>
+  );
+};
 
-}
-
-
-export default Calendar
+export default Calendar;
